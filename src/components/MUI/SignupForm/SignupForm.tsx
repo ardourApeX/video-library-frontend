0705df8b3726851f2213c2d290c46f2ba41a01d4
ardourApeX <joygupta501@gmail.com>
2022-05-27 02:14:18 +0530

    Refactor : Optimised code of signupForm

---
+++ src/components/MUI/SignupForm/SignupForm.tsx
@@ -0,0 +1,158 @@
+//React
+import { ReactElement, useState } from "react";
+
+//MUI Components
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ISignupFormError } from "../../../types/SignupForm";
+
+//TODO :
+//1. Change href for credits
+function Copyright(props: any) {
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			{...props}
+		>
+			{"Copyright © "}
+			<Link color="inherit" href="https://mui.com/">
+				Video Library
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
+}
+
+const theme = createTheme();
+
+export default function SignupForm(props: any): ReactElement {
+	const [fieldErrors, setFieldErrors] = useState<ISignupFormError>({
+		name: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const { handleSubmit } = props;
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+							handleSubmit(event, setFieldErrors)
+						}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									error={fieldErrors.name.length > 0}
+									helperText={fieldErrors.name}
+									required
+									fullWidth
+									id="name"
+									label="Name"
+									name="name"
+									autoComplete="family-name"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									error={fieldErrors.email.length > 0}
+									helperText={fieldErrors.email}
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									error={fieldErrors.password.length > 0}
+									helperText={fieldErrors.password}
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									error={fieldErrors.confirmPassword.length > 0}
+									helperText={fieldErrors.confirmPassword}
+									required
+									fullWidth
+									name="confrimPassword"
+									label="Confirm Password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+								/>
+							</Grid>
+							<Grid style={{ textAlign: "left" }} item xs={12}>
+								<FormControlLabel
+									control={
+										<Checkbox value="allowExtraEmails" color="primary" />
+									}
+									label="Show Password"
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<Link href="#" variant="body2">
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 5 }} />
+			</Container>
+		</ThemeProvider>
+	);
+}
---
+++ src/components/Signup/Signup.tsx
@@ -0,0 +1,18 @@
+import { ReactElement, useState } from "react";
+import SignupForm from "../MUI/SignupForm/SignupForm";
+import { signupFormHandler } from "../../handlers/signupFormHandler";
+
+function apiCall() {
+	console.log("Yes api call has been invoked");
+}
+
+export default function SignUp(): ReactElement {
+	//Creating a closure to handle the form submission
+	const handleSubmit = signupFormHandler(apiCall);
+
+	return (
+		<>
+			<SignupForm handleSubmit={handleSubmit} />
+		</>
+	);
+}
